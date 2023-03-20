20186c10bd4b2d71222777d0f937cbec910dbe2a
weaam3 <108591925+weaam3@users.noreply.github.com>
2023-03-21 00:33:17 +0300

    init

--- src/test/java/com/example/dbcontroller/DbControllerApplicationTests.java
+++ src/test/java/com/example/dbcontroller/DbControllerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DbControllerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
